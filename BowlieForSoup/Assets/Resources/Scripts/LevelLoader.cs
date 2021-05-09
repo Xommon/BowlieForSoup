a03fe7cd6e395335b408a3ecf826e79b4f3d3df2
Micheal Quentin <mquenti3@gmail.com>
2021-05-09 04:41:29 -0500

    Transition Animation

---
+++ BowlieForSoup/Assets/Resources/Scripts/LevelLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1.1f;
+    public GameManager gameManager;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            // Skip the loading screen
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            transition.gameObject.SetActive(true);
+            transition.Play("blocktransition_end");
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
